d76f699fa13536c8a83e3141dd4d28a633eefe98
doniamaaty24-spec <doniamaaty24@gmail.com>
2026-08-24 00:15:34 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,28 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-س
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+
+import AboutView from './views/AboutView.vue'
+import ContactView from './views/ContactView.vue'
+import FaqView from './views/FaqView.vue'
+import RequestsView from './views/RequestsView.vue'
+
+const routes = [
+  { path: '/', redirect: '/requests' },
+  { path: '/requests', name: 'requests', component: RequestsView },
+  { path: '/about', name: 'about', component: AboutView },
+  { path: '/contact', name: 'contact', component: ContactView },
+  { path: '/faq', name: 'faq', component: FaqView }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
