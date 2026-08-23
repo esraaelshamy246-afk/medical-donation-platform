397fc80edb83d3f346eba14e9e5bce5947c76ad1
doniamaaty24-spec <doniamaaty24@gmail.com>
2026-08-24 00:15:09 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,6 @@
 <script setup>
 import RequestsView from './views/RequestsView.vue'
 </script>
+<router-link to="/about">About Us</router-link>
+<router-link to="/contact">Contact Us</router-link>
+<router-link to="/faq">FAQ</router-link>
